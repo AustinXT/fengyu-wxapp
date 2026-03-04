8825c93fcbd17215504aa62b5b5e40fcb0754a27
NightVoyager <nightvoyager@qq.com>
2026-03-04 20:02:49 +0800

    fix: 更新 cart

--- fengyu-client/miniprogram/pages/cart/cart.ts
+++ fengyu-client/miniprogram/pages/cart/cart.ts
@@ -1,7 +1,7 @@
 // pages/cart/cart.ts
 Page({
   data: {
-    introUrl: 'https://636c-cloud1-3gpht4b01ff88838-1406056527.tcb.qcloud.la/images/intro.png',
+    introUrl: 'https://636c-cloud1-3gpht4b01ff88838-1406056527.tcb.qcloud.la/images/fengyuguan.jpg',
   },
 
   onShareAppMessage() {
