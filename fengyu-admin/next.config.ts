a1dcf21aa92ea81181626f57c0f0ac4f6ddf1c37
NightVoyager <nightvoyager@qq.com>
2026-03-14 00:23:29 +0800

    chore: 更新管理后台配置

--- fengyu-admin/next.config.ts
+++ fengyu-admin/next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ['postgres'],
+  webpack: (config) => {
+    config.resolve.alias['@db'] = path.resolve(__dirname, '../db/schema');
+    return config;
+  },
 };
 
 export default nextConfig;
