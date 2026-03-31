58addc44746d44b51ac4cd32ebb7e1f85da803b7
NightVoyager <nightvoyager@qq.com>
2026-03-31 17:48:41 +0800

    fix(staff-api): 服务单查询支持 is_presale 字段

--- fengyu-staff/cloudfunctions/staffApi/routes/service.js
+++ fengyu-staff/cloudfunctions/staffApi/routes/service.js
@@ -175,23 +175,28 @@ async function create(ctx) {
     for (const item of normalizedItems) {
       const serviceItemId = generateServiceItemId()
 
-      // 获取 sale_item 的 sku_id 和 unit_real_price
+      // 获取 sale_item 的 sku_id、unit_real_price 和订单类型（售前/售后判定）
       const siRows = await client.query(
-        'SELECT sku_id, unit_real_price FROM sale_items WHERE sale_item_id = $1',
+        `SELECT si.sku_id, si.unit_real_price, so.sale_order_type
+         FROM sale_items si
+         JOIN sale_orders so ON so.sale_order_id = si.sale_order_id
+         WHERE si.sale_item_id = $1`,
         [item.saleItemId]
       )
       const skuId = siRows.rows[0]?.sku_id || null
       const unitRealPrice = siRows.rows[0]?.unit_real_price || null
+      const isPresale = siRows.rows[0]?.sale_order_type === '体验'
 
       await client.query(
         `INSERT INTO service_items
-           (service_item_id, sale_item_id, unit_real_price, service_order_id,
+           (service_item_id, sale_item_id, unit_real_price, is_presale, service_order_id,
             sku_id, session_used, employee_id, service_duration)
-         VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`,
+         VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)`,
         [
           serviceItemId,
           item.saleItemId,
           unitRealPrice,
+          isPresale,
           serviceOrderId,
           skuId,
           item.sessionUsed,
@@ -423,7 +428,8 @@ async function list(ctx) {
         sli.sku_spec_name,
         sli.remaining_sessions,
         sli.session_count,
-        si.service_duration
+        si.service_duration,
+        si.is_presale
       FROM service_items si
       LEFT JOIN sale_items sli ON si.sale_item_id = sli.sale_item_id
       WHERE si.service_order_id = ANY($1)
@@ -438,6 +444,7 @@ async function list(ctx) {
       spec: i.sku_spec_name || '',
       remainingSessions: i.remaining_sessions,
       totalSessions: i.session_count,
+      isPresale: i.is_presale,
     })
   }
 
@@ -546,6 +553,7 @@ async function detail(ctx) {
       si.sale_item_id,
       si.session_used,
       si.service_duration,
+      si.is_presale,
       sli.session_count,
       sli.remaining_sessions,
       sli.sku_spec_name,
@@ -607,6 +615,7 @@ async function detail(ctx) {
       serviceDuration: i.service_duration,
       remainingSessions: i.remaining_sessions,
       totalSessions: i.session_count,
+      isPresale: i.is_presale,
     }))
   }
 }
